edf784ee44083b6cb12aaf1230130375696f2a17
olarteViviana <olarteviviana.a@gmail.com>
2023-04-24 00:16:54 -0500

    ci

---
+++ server/api-rest-rappi/test_db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+(async ()=>{
+  try{
+      mongoose.set('strictQuery', true)
+      const db = await mongoose.connect('mongodb://127.0.0.1:27017/rappi')
+      console.log("DB connected", db.connection.name )
+      process.exit();
+  }
+  catch (error){
+      console.error(error)
+  }
+  
+})()
